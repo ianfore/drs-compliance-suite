f0f651e8db59aa589a8d0bdb52dc3cb19e0b719f
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2022-09-28 15:49:27 -0400

    update constants

--- compliance_suite/constants.py
+++ compliance_suite/constants.py
@@ -2,5 +2,3 @@ SCHEMA_FILE_PATH = "compliance_suite/schemas/"
 SCHEMA_DRS_OBJECT = "drs_object.json"
 SCHEMA_ERROR = "error.json"
 SCHEMA_SERVICE_INFO = "service_info.json"
-DRS_OBJECT_ID_PRESENT = "697907bf-d5bd-433e-aac2-1747f1faf366" # drs object present in the drs server
-DRS_OBJECT_ID_ABSENT = "697907bf-d5bd-433e-aac2-1747f1faf377" # drs object absent in the drs server
