2f63dc6d710b8e27ab2b177b281f5fb8cc796744
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2022-10-21 16:03:21 -0400

    use ga4gh-testbed-lib for report generation

--- compliance_suite/constants.py
+++ compliance_suite/constants.py
@@ -1,3 +1,8 @@
-SCHEMA_DRS_OBJECT = "drs_object.json"
-SCHEMA_ERROR = "error.json"
-SCHEMA_SERVICE_INFO = "service_info.json"
+DRS_OBJECT_SCHEMA = "drs_object.json"
+ERROR_SCHEMA = "error.json"
+SERVICE_INFO_SCHEMA = "service_info.json"
+TESTBED_NAME = "DRS Compliance Suite",
+TESTBED_VERSION = "v0.0.0",
+TESTBED_DESCRIPTION = "Test the compliance of a DRS implementation with GA4GH DRS v1.2.0 specification",
+SERVICE_INFO_URL = "/service-info"
+DRS_OBJECT_INFO_URL = "/objects/"
